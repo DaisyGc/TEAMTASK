b42c5eb4d04f699ed331574feb80015d4d25e726
DaisyGc <916842503@qq.com>
2021-04-07 17:50:16 +0800

    重新生成了一个git，提交自己的游戏

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//相机视角跟踪
+public class FollowPlayer : MonoBehaviour
+{
+
+    //游戏人物 的位置
+    private Transform player;
+
+    //游戏人物与 相机的差
+    private Vector3 offset;
+
+    //相机的速度
+    private float smoothing = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //player.TransformDirection(offset)
+        //世界坐标转换为局部坐标
+        Vector3 targetPosition = player.position + player.TransformDirection(offset);
+        //Vector3.Lerp 计算相机位置 和 目标位置的插值
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothing);
+        //相机的目标看向 游戏人物
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //private float rotateSpeed = 30f;
+    private float movespeed = 5;
+
+    void LateUpdate()
+    {
+        //第一种控制移动
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //朝一个方向移动 new Vector3(0, 0, v) * speed * Time.deltaTime是个向量
+        transform.Translate(new Vector3(0, 0, v) * movespeed * Time.deltaTime);  //前后移动
+
+        transform.Translate(new Vector3(h, 0, 0) * movespeed * Time.deltaTime); //左右旋转
+
+        //        //第二种方式控制移动
+        //        if (Input.GetKey(KeyCode.W)) //前进
+        //        {
+        //            transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+        //        }
+        //        if (Input.GetKey(KeyCode.S)) //后退
+        //        {
+        //            transform.Translate(Vector3.back * movespeed * Time.deltaTime);
+        //        }
+        //        if (Input.GetKey(KeyCode.A))//向左旋转
+        //        {
+        //            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        //        }
+        //        if (Input.GetKey(KeyCode.D))//向右旋转
+        //　　{
+        //            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        //        }
+    }
+}
